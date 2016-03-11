fe552f1af6a2acd8c88e6b5b5c3c552de08c2c03
naomijub <jnboeira@outlook.com>
2016-03-11 00:56:34 -0300

    memento pattern

---
+++ MementoPattern/MementoPattern/Originator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MementoPattern
+{
+    public class Originator
+    {
+        public string str1 { get; set; }
+        public string str2 { get; set; }
+        private Memento memento;
+
+        public Originator(string str1, string str2) {
+            this.str1 = str1;
+            this.str2 = str2;
+        }
+
+        public void setStr(string str1, string str2) {
+            this.str1 = str1;
+            this.str2 = str2;
+        }
+
+        public void saveMemento() {
+            this.memento = new Memento(str1, str2);
+        }
+
+        public Memento getMemento() {
+            return memento;
+        }
+
+        public class Memento
+        {
+            public readonly string str1;
+            public readonly string str2;
+
+            public Memento(string str1, string str2)
+            {
+                this.str1 = str1;
+                this.str2 = str2;
+            }
+
+            public string getStr1() { return str1; }
+            public string getStr2() { return str2; }
+        }
+    }
+}
---
+++ MementoPattern/MementoPattern/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MementoPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<Originator.Memento> savedStates = new List<Originator.Memento>();
+
+            Originator originator = new Originator("A", "B");
+            originator.saveMemento();
+            savedStates.Add(originator.getMemento());
+
+            originator.setStr("C", "D");
+            originator.saveMemento();
+            savedStates.Add(originator.getMemento());
+
+            originator.setStr("E", "F");
+            originator.saveMemento();
+            savedStates.Add(originator.getMemento());
+
+            foreach (Originator.Memento mem in savedStates) {
+                Console.WriteLine(mem.getStr1() + " " + mem.getStr2());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
